42f4f28e886e7cf7d7b665e36d478a08cb7e2e29
Stacy <smbridson@gmail.com>
2018-08-15 09:25:20 -0400

    added second fedora cat

--- cat.js
+++ cat.js
@@ -7,3 +7,13 @@ function myCatClicker(){
     document.getElementById('counter').innerHTML = clicker();
     console.log(counter);
 }
+
+let counter2 = 1;
+const clicker2 = (function(){
+        return counter2 ++;
+})
+
+function myCatClicker2(){
+    document.getElementById('counter2').innerHTML = clicker2();
+    console.log(counter2);
+}
